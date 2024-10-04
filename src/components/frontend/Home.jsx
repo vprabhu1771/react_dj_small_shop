266060bfe72cbc4f6867e6d82a7194e8d58823ed
vprabhu1771 <vprabhu1771@gmail.com>
2024-10-04 18:29:58 +0530

    Product List Page added

--- src/components/frontend/Home.jsx
+++ src/components/frontend/Home.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { apiRequest } from '../../services/api';
+
 const Home = () => {
-  return (
-    <div>
-      {/* Home content */}
-      Home
-    </div>
-  );
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+          const response = await apiRequest(`${import.meta.env.VITE_API_BASE_URL}/products`);
+          setProducts(response.data);
+            console.log(response.data);
+        
+        };
+    
+        fetchProducts();
+      }, []);
+
+    return (
+        <div>
+            {products.map((row) => (
+                <div key={row.id}>
+                <h3>{row.name}</h3>
+                <p>{row.price}</p>
+                <button>Add to Cart</button>
+                </div>
+            ))}
+        </div>
+    );
 };
 export default Home;
