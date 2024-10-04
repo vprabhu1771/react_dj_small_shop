80b3d9e7c907734f4b6e27650ebe425aff24de8c
vprabhu1771 <vprabhu1771@gmail.com>
2024-10-04 18:16:45 +0530

    Create AppLayout.jsx

---
+++ src/components/frontend/layout/AppLayout.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import TopBar from './TopBar';
+import Header from './Header';
+import Footer from './Footer';
+const AppLayout = ({ children }) => {
+  return (
+    <div>
+      <TopBar />
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  );
+};
+export default AppLayout;
